80427a16e4d00361708829e1982d919aa6e54c56
Gabriel-Rodrigues01 <163764950+Gabriel-Rodrigues01@users.noreply.github.com>
2025-10-04 15:44:28 -0300

    correçoes no pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,11 @@ module org.cadastro_funcionarios {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens org.cadastro_funcionarios to javafx.fxml;
-    exports org.cadastro_funcionarios;
-    exports org.cadastro_funcionarios.application_controller;
     opens org.cadastro_funcionarios.application_controller to javafx.fxml;
+
+    exports org.cadastro_funcionarios.application_controller;
+    exports org.cadastro_funcionarios.model;
+    exports org.cadastro_funcionarios.repository;
+    exports org.cadastro_funcionarios.service;
+    exports org.cadastro_funcionarios.util;
 }
